0e289d7138855443329793f780f7515a7ffe7298
Daniyal Dolare <daniyal.dolare@gmail.com>
2020-12-04 12:52:11 +0530

    shifted database to cloud firestore

--- lib/database.dart
+++ lib/database.dart
@@ -1,72 +1,184 @@
 import 'package:example/todo.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_database/firebase_database.dart';
 import 'note.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-final databaseReference = FirebaseDatabase.instance.reference();
+// final databaseReference = FirebaseDatabase.instance.reference();
 final user = FirebaseAuth.instance.currentUser.uid;
 
-String saveNote(Note note) {
-  var id = databaseReference.child(user).child("notes/").push();
-  id.set(note.toJson());
-  print(id.key);
-  return id.key;
+// String saveNote(Note note) {
+//   var id = databaseReference.child(user).child("notes/").push();
+//   id.set(note.toJson());
+//   print(id.key);
+//   return id.key;
+// }
+
+// void updateNote(Note note, String id) {
+//   databaseReference.child(user).child("notes/" + id).update(note.toJson());
+// }
+
+// Future<List<Note>> getNotes() async {
+//   DataSnapshot dataSnapshot =
+//       await databaseReference.child(user).child('notes/').once();
+//   List<Note> notes = [];
+
+//   if (dataSnapshot.value != null) {
+//     Map<dynamic, dynamic> values = dataSnapshot.value;
+//     values.forEach((key, value) {
+//       Note note =
+//           Note(title: value['title'], note: value['note'], time: value['time']);
+//       note.setId(key.toString());
+//       notes.add(note);
+//     });
+//   } else {
+//     print('No data');
+//   }
+//   return notes;
+// }
+
+// Future<bool> deleteNote(Note note) async {
+//   await databaseReference.child(user).child('notes/').child(note.id).remove();
+//   return true;
+// }
+
+// String saveTodo(Todo todo) {
+//   var id = databaseReference.child(user).child('todos/').push();
+//   id.set(todo.toJson());
+//   return id.key;
+// }
+
+// void updateTodo(Todo todo) {
+//   databaseReference.child(user + '/todos').child(todo.id).update(todo.toJson());
+// }
+
+// Future<void> deleteTodo(Todo todo) async {
+//   await databaseReference.child(user).child('todos/').child(todo.id).remove();
+// }
+
+// Future<List<Todo>> getTodos() async {
+//   DataSnapshot dataSnapshot =
+//       await databaseReference.child(user + '/todos').once();
+
+//   List<Todo> todos = [];
+//   if (dataSnapshot.value != null) {
+//     Map<dynamic, dynamic> values = dataSnapshot.value;
+//     values.forEach((key, value) {
+//       Todo todo = Todo(title: value['title'], isChecked: value['isChecked']);
+//       todo.id = key.toString();
+//       todos.add(todo);
+//     });
+//   }
+//   return todos;
+// }
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+Future<String> saveNoteToFirestore(Note note) async {
+  var id = firestore
+      .collection(user)
+      .doc('data')
+      .collection('notes')
+      .add(note.toJson())
+      .catchError((error) => print('this is the error: $error'));
+  return await id.then((value) => value.id);
 }
 
-void updateNote(Note note, String id) {
-  databaseReference.child(user).child("notes/" + id).update(note.toJson());
-}
+Future<List<Note>> getNotesFromFirestore() async {
+  QuerySnapshot dataSnapshot = await firestore
+      .collection(user)
+      .doc('data')
+      .collection('notes')
+      .get(GetOptions(source: Source.serverAndCache));
 
-Future<List<Note>> getNotes() async {
-  DataSnapshot dataSnapshot =
-      await databaseReference.child(user).child('notes/').once();
   List<Note> notes = [];
 
-  if (dataSnapshot.value != null) {
-    Map<dynamic, dynamic> values = dataSnapshot.value;
-    values.forEach((key, value) {
+  if (dataSnapshot.docs.isNotEmpty) {
+    for (QueryDocumentSnapshot values in dataSnapshot.docs) {
+      // print(values.id);
+      // print(values.reference);
+      var id = values.id;
+      var data = values.data();
       Note note =
-          Note(title: value['title'], note: value['note'], time: value['time']);
-      note.setId(key.toString());
+          Note(title: data['title'], note: data['note'], time: data['time']);
+      note.setId(id.toString());
       notes.add(note);
-    });
+    }
   } else {
     print('No data');
   }
   return notes;
 }
 
-Future<bool> deleteNote(Note note) async {
-  await databaseReference.child(user).child('notes/').child(note.id).remove();
+Future<void> updateNoteToFirestore(Note note, String id) async {
+  await firestore
+      .collection(user)
+      .doc('data')
+      .collection('notes')
+      .doc(id)
+      .update(note.toJson())
+      .catchError((error) => print('this is yhe error: $error'));
+}
+
+Future<bool> deleteNoteFromFirestore(Note note) async {
+  await firestore
+      .collection(user)
+      .doc('data')
+      .collection('notes')
+      .doc(note.id)
+      .delete();
   return true;
 }
 
-String saveTodo(Todo todo) {
-  var id = databaseReference.child(user).child('todos/').push();
-  id.set(todo.toJson());
-  return id.key;
+Future<String> saveTodoToFirestore(Todo todo) async {
+  var id = firestore
+      .collection(user)
+      .doc('data')
+      .collection('todo')
+      .add(todo.toJson())
+      .catchError((error) => print('this is the error: $error'));
+  return await id.then((value) => value.id);
 }
 
-void updateTodo(Todo todo) {
-  databaseReference.child(user + '/todos').child(todo.id).update(todo.toJson());
+void updateTodoToFirestore(Todo todo) async {
+  await firestore
+      .collection(user)
+      .doc('data')
+      .collection('todo')
+      .doc(todo.id)
+      .update(todo.toJson())
+      .catchError((error) => print('this is the error: $error'));
 }
 
-Future<void> deleteTodo(Todo todo) async {
-  await databaseReference.child(user).child('todos/').child(todo.id).remove();
+Future<void> deleteTodoFromFirestore(Todo todo) async {
+  await firestore
+      .collection(user)
+      .doc('data')
+      .collection('todo')
+      .doc(todo.id)
+      .delete()
+      .catchError((error) => print('this is the error: $error'));
 }
 
-Future<List<Todo>> getTodos() async {
-  DataSnapshot dataSnapshot =
-      await databaseReference.child(user + '/todos').once();
+Future<List<Todo>> getTodosFromFirestore() async {
+  QuerySnapshot querySnapshot = await firestore
+      .collection(user)
+      .doc('data')
+      .collection('todos')
+      .get(GetOptions(source: Source.serverAndCache));
 
   List<Todo> todos = [];
-  if (dataSnapshot.value != null) {
-    Map<dynamic, dynamic> values = dataSnapshot.value;
-    values.forEach((key, value) {
-      Todo todo = Todo(title: value['title'], isChecked: value['isChecked']);
-      todo.id = key.toString();
+
+  if (querySnapshot.docs.isNotEmpty) {
+    for (QueryDocumentSnapshot values in querySnapshot.docs) {
+      var id = values.id;
+      var data = values.data();
+      Todo todo = Todo(title: data['title'], isChecked: data['isChecked']);
+      todo.id = id.toString();
       todos.add(todo);
-    });
+    }
+  } else {
+    print('no data');
   }
   return todos;
 }
