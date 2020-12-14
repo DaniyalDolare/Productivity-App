de3c187b1525c3875df12ced685a67762359e00f
Daniyal Dolare <daniyal.dolare@gmail.com>
2020-12-14 10:50:48 +0530

    Displaying app usages with app icon and name

--- lib/add_data.dart
+++ lib/add_data.dart
@@ -1,5 +1,6 @@
-import 'package:app_usage/app_usage.dart';
 import 'package:flutter/material.dart';
+import 'package:device_apps/device_apps.dart';
+import 'app_usage.dart';
 
 class AppUsages extends StatefulWidget {
   @override
@@ -7,21 +8,50 @@ class AppUsages extends StatefulWidget {
 }
 
 class _AppUsagesState extends State<AppUsages> {
-  List<AppUsageInfo> _infos = [];
+  List _infos = [];
   TextEditingController startdate = TextEditingController();
 
   void getUsageStats() async {
     DateTime startDate = DateTime.parse(startdate.text);
     try {
+      _infos = [];
       // DateTime startDate = DateTime(
       //     DateTime.now().year, DateTime.now().month, DateTime.now().day);
-
+      Map<String, dynamic> appData = Map();
       DateTime endDate = new DateTime.now();
-      List<AppUsageInfo> infos = await AppUsage.getAppUsage(startDate, endDate);
-      setState(() {
-        _infos = infos;
+      List<AppUsageInfo> appsWithUsage =
+          await AppUsage.getAppUsage(startDate, endDate);
+      appsWithUsage.sort((a, b) {
+        return b.usage.compareTo(a.usage);
       });
-    } on AppUsageException catch (exception) {
+      print(appsWithUsage.length);
+
+      List<Application> installedApps =
+          await DeviceApps.getInstalledApplications(
+              includeAppIcons: true,
+              includeSystemApps: true,
+              onlyAppsWithLaunchIntent: true);
+      print(installedApps.length);
+
+      for (var appWithUsage in appsWithUsage) {
+        if (appWithUsage.usage < Duration(minutes: 2)) {
+          continue;
+        }
+        Application app =
+            await DeviceApps.getApp(appWithUsage.packageName, true);
+        if (app is ApplicationWithIcon) {
+          appData = {
+            'name': app.appName,
+            'icon': app.icon,
+            'usage': appWithUsage.usage
+          };
+          _infos.add(appData);
+        }
+      }
+
+      print(_infos.length);
+      setState(() {});
+    } catch (exception) {
       print(exception);
     }
   }
@@ -47,9 +77,18 @@ class _AppUsagesState extends State<AppUsages> {
                 itemCount: _infos.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                      title: Text(
-                          "com.${_infos[index].packageName}.${_infos[index].appName}"),
-                      trailing: Text(_infos[index].usage.toString()));
+                    contentPadding: EdgeInsets.all(8.0),
+                    // title: Text(
+                    //     "com.${_infos[index].packageName}.${_infos[index].appName}"),
+                    // trailing: Text(_infos[index].usage.toString()));
+                    leading: CircleAvatar(
+                      radius: 25.0,
+                      backgroundImage: MemoryImage(_infos[index]['icon']),
+                      backgroundColor: Colors.transparent,
+                    ),
+                    title: Text(_infos[index]['name']),
+                    trailing: Text(_infos[index]['usage'].toString()),
+                  );
                 }),
           ),
         ],
