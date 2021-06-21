df2f76ebb0a135fdb4654e1594da2e4e71b4fc5c
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-06-22 00:31:36 +0530

    login converted to stateless

--- lib/login.dart
+++ lib/login.dart
@@ -1,43 +1,39 @@
 import 'package:example/screens/home%20screen/tabs.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'services/auth.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  User user;
-
-  void click() {
-    signInWithGoogle().then((user) => {
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => Tabs()))
-        });
-  }
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-              primary: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(45),
-              ),
-              side: BorderSide(color: Colors.redAccent)),
-          onPressed: () {
-            click();
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text("Login with Google"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
           ),
-        ),
+          Center(
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  side: BorderSide(color: Colors.redAccent)),
+              onPressed: () async {
+                await signInWithGoogle().then((user) => {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Tabs()))
+                    });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("Login with Google"),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
